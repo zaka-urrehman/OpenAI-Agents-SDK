48a1cc0d503a98cb75a4cb83b655c10fe2b21139
Zaka Urrehman <zk.na2486@gmail.com>
2025-06-23 18:56:36 +0500

    feat: add handoffs step

---
+++ 05_handoffs/customizing_handoff.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, handoff, RunContextWrapper
+from agents.run import RunConfig
+import asyncio
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+def on_handoff(agent: Agent, ctx: RunContextWrapper[None]):
+    agent_name = agent.name
+    print("--------------------------------")
+    print(f"Handing off to {agent_name}...")
+    print("--------------------------------")
+
+
+urdu_agent = Agent(
+    name="Urdu Agent",
+    instructions="You are a helpful assistant that can answer questions in Urdu.",
+    model=model
+)
+
+arabic_agent = Agent(
+    name="Arabic Agent",
+    instructions="You are a helpful assistant that can answer questions in Arabic.",
+    model=model
+)
+
+async def main():
+    agent = Agent(
+        name="Assistant",
+        instructions="You are a helpful assistant that greets users when they say hello. You can also handoff to the Urdu Agent or the Arabic Agent to answer questions in Urdu or Arabic.",
+        model=model,
+        handoffs=[
+            handoff(urdu_agent, on_handoff=lambda ctx: on_handoff(urdu_agent, ctx)),
+            handoff(arabic_agent, on_handoff=lambda ctx: on_handoff(arabic_agent, ctx))
+        ]
+    )
+
+    result = await Runner.run(agent, "Salam. kya hal hy?", run_config=config)
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
